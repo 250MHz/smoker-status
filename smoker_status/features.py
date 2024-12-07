b4027d832e13bd4c6392071a72cd090a988007e5
250MHz <250MHz@users.noreply.github.com>
2024-12-07 05:39:05 -0800

    Undo encoding of urine protein
    Urine protein dipstick test results are not nominal, it's ordinal
    because the different test values can be ordered (a score of 1+ is less
    protein than a score of 2+).
    
    So it doesn't make sense to do one hot encoding here.
    See https://stats.stackexchange.com/a/384307

--- smoker_status/features.py
+++ smoker_status/features.py
@@ -1,35 +1,29 @@
-from pandas import concat, DataFrame
+import numpy as np
+import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 
 
-def create_encoded_X(X: DataFrame) -> DataFrame:
+def create_encoded_X(X: pd.DataFrame) -> pd.DataFrame:
     """Performs one hot encoding on X and returns a new data frame."""
     cat_feats = [
         'hearing(left)',
         'hearing(right)',
-        'Urine protein',
         'dental caries',
     ]
     X_no_cat_feat = X.drop(cat_feats, axis=1)
     X_only_cat_feat = X[cat_feats]
 
-    enc = OneHotEncoder()
+    enc = OneHotEncoder(dtype=np.int64)
     enc.fit(X_only_cat_feat)
-    X_only_cat_feat_trans = DataFrame(
+    X_only_cat_feat_trans = pd.DataFrame(
         data=enc.transform(X_only_cat_feat).toarray(),
         columns=[
             'hearing(left) - normal',
             'hearing(left) - abnormal',
             'hearing(right) - normal',
             'hearing(right) - abnormal',
-            'Urine protein - negative',
-            'Urine protein - trace',
-            'Urine protein - 1+',
-            'Urine protein - 2+',
-            'Urine protein - 3+',
-            'Urine protein - 4+',
             'dental caries - nonpresent',
             'dental caries - present',
         ],
     )
-    return concat([X_no_cat_feat, X_only_cat_feat_trans], axis=1)
+    return pd.concat([X_no_cat_feat, X_only_cat_feat_trans], axis=1)
